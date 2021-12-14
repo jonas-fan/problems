25bc92cd9c3e3e99e512ec6222b2a57331729d70
Jonas Fan <jianfan.tw@gmail.com>
2021-12-14 21:57:03 +0800

    Create 1281-subtract-the-product-and-sum-of-digits-of-an-integer.go

---
+++ leetcode/1281-subtract-the-product-and-sum-of-digits-of-an-integer.go
@@ -0,0 +1,14 @@
+func subtractProductAndSum(n int) int {
+    sum := 0
+    product := 1
+
+    for n > 0 {
+        digit := n % 10
+
+        sum += digit
+        product *= digit        
+        n /= 10
+    }
+
+    return product - sum
+}
