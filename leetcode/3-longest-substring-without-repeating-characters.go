95e9f8a97644476b880ac825199a5253a53f8143
Jonas Fan <jianfan.tw@gmail.com>
2022-10-23 21:40:32 +0800

    Update 3-longest-substring-without-repeating-characters.go

--- leetcode/3-longest-substring-without-repeating-characters.go
+++ leetcode/3-longest-substring-without-repeating-characters.go
@@ -1,24 +1,24 @@
-func lengthOfLongestSubstring(s string) int {
-    longest := 0
-    begin := -1
-    seen := map[rune]int{}
-
-    for index, each := range s {
-        if at, ok := seen[each]; ok {
-            begin = max(begin, at)
-        }
-
-        seen[each] = index
-        longest = max(longest, index-begin)
-    }
-
-    return longest
-}
-
 func max(lhs int, rhs int) int {
     if lhs < rhs {
         return rhs
     }
 
     return lhs
+}
+
+func lengthOfLongestSubstring(s string) int {
+    out := 0
+    begin := -1
+    seen := make(map[rune]int)
+
+    for index, letter := range s {
+        if where, ok := seen[letter]; ok {
+            begin = max(begin, where)
+        }
+
+        seen[letter] = index
+        out = max(out, index-begin)
+    }
+
+    return out
 }
