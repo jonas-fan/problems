b7a444dd5b1c3f584d3045b5ed708e7a6f3f5e96
Jonas Fan <jianfan.tw@gmail.com>
2022-10-29 10:57:26 +0800

    Create 700-search-in-a-binary-search-tree.go

---
+++ leetcode/700-search-in-a-binary-search-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    if root.Val < val {
+        return searchBST(root.Right, val)
+    } else if root.Val > val {
+        return searchBST(root.Left, val)
+    }
+
+    return root
+}
