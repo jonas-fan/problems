06edc11ce3d9e51600f4ea130df17e4089d5bb64
Jonas Fan <jianfan.tw@gmail.com>
2022-11-03 21:06:53 +0800

    Create 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.go

---
+++ leetcode/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.go
@@ -0,0 +1,15 @@
+// #string
+
+import (
+    "strings"
+)
+
+func isPrefixOfWord(sentence string, searchWord string) int {
+    for i, word := range strings.Split(sentence, " ") {
+        if strings.HasPrefix(word, searchWord) {
+            return i + 1
+        }
+    }
+
+    return -1
+}
