073c750f3c47408423e77fc94bf6faaaf83b8e95
Jonas Fan <jianfan.tw@gmail.com>
2022-11-09 22:00:38 +0800

    Update 1480-running-sum-of-1d-array.go

--- leetcode/1480-running-sum-of-1d-array.go
+++ leetcode/1480-running-sum-of-1d-array.go
@@ -1,10 +1,14 @@
+// #array
+
 func runningSum(nums []int) []int {
     out := make([]int, len(nums))
-    sum := 0
 
-    for index, each := range nums {
-        out[index] = each + sum
-        sum = out[index]
+    if len(out) > 0 {
+        out[0] = nums[0]
+
+        for i := 1; i < len(out); i++ {
+            out[i] = nums[i] + out[i-1]
+        }
     }
 
     return out
