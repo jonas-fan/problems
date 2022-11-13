55597daaba0888a12d71583101c81477b05f9b42
Jonas Fan <jianfan.tw@gmail.com>
2022-11-13 16:11:46 +0800

    Create 36-valid-sudoku.go

---
+++ leetcode/36-valid-sudoku.go
@@ -0,0 +1,87 @@
+// #array
+
+func validHorizontal(board [][]byte, row int, col int) bool {
+    seen := [9]int{}
+
+    for ; col < 9; col++ {
+        num := board[row][col]
+
+        if num == '.' {
+            continue
+        }
+
+        if seen[num-'1'] > 0 {
+            return false
+        }
+
+        seen[num-'1']++
+    }
+
+    return true
+}
+
+func validVertical(board [][]byte, row int, col int) bool {
+    seen := [9]int{}
+
+    for ; row < 9; row++ {
+        num := board[row][col]
+
+        if num == '.' {
+            continue
+        }
+
+        if seen[num-'1'] > 0 {
+            return false
+        }
+
+        seen[num-'1']++
+    }
+
+    return true
+}
+
+func validSquare(board [][]byte, row int, col int) bool {
+    seen := [9]int{}
+
+    for i := row; i < row + 3; i++ {
+        for j := col; j < col + 3; j++ {
+            num := board[i][j]
+
+            if num == '.' {
+                continue
+            }
+
+            if seen[num-'1'] > 0 {
+                return false
+            }
+
+            seen[num-'1']++
+        }
+    }
+
+    return true
+}
+
+func isValidSudoku(board [][]byte) bool {
+    for row := 0; row < len(board); row++ {
+        if !validHorizontal(board, row, 0) {
+            return false
+        }
+    }
+
+    for col := 0; col < len(board[0]); col++ {
+        if !validVertical(board, 0, col) {
+            return false
+        }
+    }
+
+    for row := 0; row < len(board); row += 3 {
+        for col := 0; col < len(board[row]); col += 3 {
+            if !validSquare(board, row, col) {
+                return false
+            }
+        }
+    }
+
+    return true
+}
