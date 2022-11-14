c00f69b0b7ec1c990d53ec0c5cf5a1c568caddb1
Jonas Fan <jianfan.tw@gmail.com>
2022-11-14 13:38:33 +0800

    Update 387-first-unique-character-in-a-string.go

--- leetcode/387-first-unique-character-in-a-string.go
+++ leetcode/387-first-unique-character-in-a-string.go
@@ -1,17 +1,23 @@
 // #string
 
-func firstUniqChar(s string) int {
-    seen := map[rune]int{}
+func counts(chars string) [256]int {
+    out := [256]int{}
 
-    for _, char := range s {
-        seen[char]++
+    for _, char := range chars {
+        out[int(char)]++
     }
 
+    return out
+}
+
+func firstUniqChar(s string) int {
+    visits := counts(s)
+
     for i, char := range s {
-        if seen[char] == 1 {
+        if visits[int(char)] == 1 {
             return i
         }
     }
 
-    return -1;
+    return -1
 }
