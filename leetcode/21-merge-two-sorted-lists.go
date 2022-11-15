f734acfb8b7b29850ca93b1d71653cd125e2f686
Jonas Fan <jianfan.tw@gmail.com>
2022-11-15 16:24:30 +0800

    Update 21-merge-two-sorted-lists.go

--- leetcode/21-merge-two-sorted-lists.go
+++ leetcode/21-merge-two-sorted-lists.go
@@ -15,10 +15,10 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
     for list1 != nil || list2 != nil {
         if list1 == nil {
             node.Next = list2
-            list2 = list2.Next
+            list2 = nil
         } else if list2 == nil {
             node.Next = list1
-            list1 = list1.Next
+            list1 = nil
         } else if list1.Val < list2.Val {
             node.Next = list1
             list1 = list1.Next
