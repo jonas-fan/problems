24c6b4108e36a860b79f8a84bff18d74839b80b7
Jonas Fan <jianfan.tw@gmail.com>
2022-11-18 21:14:54 +0800

    Update 145-binary-tree-postorder-traversal.go

--- leetcode/145-binary-tree-postorder-traversal.go
+++ leetcode/145-binary-tree-postorder-traversal.go
@@ -1,3 +1,5 @@
+// #tree #binary-tree #depth-first-search
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -7,18 +9,27 @@
  * }
  */
 
-func dfs(node *TreeNode, out []int) []int {
-    if node == nil {
-        return out
+func postorderTraversal(root *TreeNode) []int {
+    out := []int{}
+    stack := []*TreeNode{}
+    last := &TreeNode{}
+
+    for root != nil || len(stack) > 0 {
+        if root != nil {
+            stack = append(stack, root)
+            root = root.Left
+        } else {
+            node := stack[len(stack)-1]
+
+            if node.Right == nil || node.Right == last {
+                stack = stack[:len(stack)-1]
+                out = append(out, node.Val)
+                last = node
+            } else {
+                root = node.Right
+            }
+        }
     }
 
-    out = dfs(node.Left, out)
-    out = dfs(node.Right, out)
-    out = append(out, node.Val)
-
     return out
-}
-
-func postorderTraversal(root *TreeNode) []int {
-    return dfs(root, []int{})
 }
