323f4196ae213804515cb8cbe7b2244816aea1e3
Jonas Fan <jianfan.tw@gmail.com>
2022-11-22 14:55:58 +0800

    Update 70-climbing-stairs.go

--- leetcode/70-climbing-stairs.go
+++ leetcode/70-climbing-stairs.go
@@ -3,10 +3,11 @@
 func climbStairs(n int) int {
     steps := make([]int, 0, n+1)
 
+    steps = append(steps, 0)
     steps = append(steps, 1)
-    steps = append(steps, 1)
+    steps = append(steps, 2)
 
-    for i := 2; i <= n; i++ {
+    for i := 3; i <= n; i++ {
         steps = append(steps, steps[i-1] + steps[i-2])
     }
 
