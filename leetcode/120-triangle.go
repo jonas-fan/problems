ec3241d886c6588129c681d0665395f36be33037
Jonas Fan <jianfan.tw@gmail.com>
2022-11-23 00:27:45 +0800

    Update 120-triangle.go

--- leetcode/120-triangle.go
+++ leetcode/120-triangle.go
@@ -1,9 +1,5 @@
 // #dynamic-programming
 
-import (
-    "math"
-)
-
 func min(lhs int, rhs int) int {
     if lhs < rhs {
         return lhs
@@ -13,28 +9,14 @@ func min(lhs int, rhs int) int {
 }
 
 func minimumTotal(triangle [][]int) int {
-    sum := make([]int, len(triangle[len(triangle)-1]))
-    sum[0] = triangle[0][0]
+    sum := triangle
 
-    for row := 0; row < len(triangle) - 1; row++ {
-        for col := len(triangle[row]); col >= 0; col-- {
-            val := triangle[row+1][col]
-
-            if col == 0 {
-                sum[col] = sum[col] + val
-            } else if col == len(triangle[row]) {
-                sum[col] = sum[col-1] + val
-            } else {
-                sum[col] = min(sum[col]+val, sum[col-1]+val)
-            }
+    for row := len(sum) - 2; row >= 0; row-- {
+        for col := 0; col < len(sum[row]); col++ {
+            sum[row][col] = min(sum[row][col] + sum[row+1][col],
+                                sum[row][col] + sum[row+1][col+1])
         }
     }
 
-    out := math.MaxInt
-
-    for _, val := range sum {
-        out = min(out, val)
-    }
-
-    return out
+    return sum[0][0]
 }
